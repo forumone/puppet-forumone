30bdb00b6e1bebe276ab2af0e4dec21b87b82432
William Hurley <whurley@forumone.com>
2016-04-13 12:29:35 -0400

    Changing to use pre-built binaries

--- manifests/ruby.pp
+++ manifests/ruby.pp
@@ -1,46 +1,44 @@
 class forumone::ruby (
-  $version = "1.9.3-p484",
+  $version = "1.9.3",
   $user    = "vagrant",
   $group   = "vagrant",
 ) {
+
+  $ruby_directory = "/home/${user}/.rbenv/versions/${version}"
+  $ruby_download = "https://s3.amazonaws.com/pkgr-buildpack-ruby/current/centos-6/ruby-${version}.tgz"
+  
   rbenv::plugin::rbenvvars { $user:
     user  => $user,
     group => $group,
     home  => "/home/${user}",
     root  => "/home/${user}/.rbenv"
   }
-
+  
+  exec { "forumone::ruby::download":
+    command => "wget --directory-prefix=/tmp/vagrant-cache ${ruby_download}",
+    path    => '/usr/bin',
+    require => [ File["/tmp/vagrant-cache"] ],
+    creates => "/tmp/vagrant-cache/ruby-${version}.tgz",
+    timeout => 4800,
+  }
+  
   rbenv::install { $user:
     user => $user,
     group => $group,
     home  => "/home/${user}",
     root => "/home/${user}/.rbenv"
-  }
-
-  rbenv::compile { $version:
-    user => $user,
-    group => $group,
-    home => "/home/${user}",
-    root => "/home/${user}/.rbenv"
-  }
-
-  file { "/home/${user}/Gemfile":
-    ensure  => present,
-    path    => "/home/${user}/Gemfile",
-    owner   => $user,
-    group   => $group,
-    content => file('/vagrant/Gemfile'),
-    backup  => false,
+  } ->
+  
+  # rbenv version
+  file { $ruby_directory:
+    ensure  => "directory"
   }
   
-  exec {"${user} bundle":
-    command   => "bundle",
-    cwd       => "/vagrant",
-    user      => $user,
-    group     => $group,
-    path      => "/home/${user}/bin:/home/${user}/.rbenv/shims:/bin:/usr/bin",
-    creates   => "/vagrant/Gemfile.lock",
-    subscribe => File["/home/${user}/Gemfile"],
-    require   => Rbenvgem["${user}/${version}/bundler/present"]
+  # extract from the ruby archive
+  exec { "forumone::ruby::extract":
+    command => "tar -zxvf /tmp/vagrant-cache/ruby-${version}.tgz -C ${ruby_directory}",
+    path    => ["/bin"],
+    require => File[$ruby_directory],
+    creates => "${ruby_directory}/bin",
   }
 }
