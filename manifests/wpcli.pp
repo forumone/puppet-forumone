31d900d4045a6f1111f6207bf9856a8efb2da16f
William Hurley <whurley@forumone.com>
2016-04-14 14:02:53 -0400

    Adding --no-check-certificate

--- manifests/wpcli.pp
+++ manifests/wpcli.pp
@@ -1,7 +1,7 @@
 class forumone::wpcli ($url = 'https://raw.githubusercontent.com/wp-cli/builds/gh-pages/phar/wp-cli.phar')
 {
   exec { 'forumone::wpcli::download':
-    command => "wget ${url} -O /usr/local/bin/wp",
+    command => "wget ${url} -O /usr/local/bin/wp --no-check-certificate",
     path    => '/usr/bin',
     creates => '/usr/local/bin/wp',
     }
