63d2964916b144cda08c4d98f74d89c53523a52d
William Hurley <whurley@forumone.com>
2016-06-03 20:39:25 -0400

    Installing g++ 4.8 through software collections

--- manifests/nodejs.pp
+++ manifests/nodejs.pp
@@ -4,6 +4,17 @@ class forumone::nodejs ($modules = ["grunt-cli", "bower"], $version = 'v4.4.1') 
     make_install => false,
   }
 
+  yumrepo { "devtools2":
+    baseurl  => "http://people.centos.org/tru/devtools-2/devtools-2.repo",
+    descr    => "Devtools2",
+    enabled  => 1,
+    gpgcheck => 0
+  }
+  
+  package { [ 'devtoolset-2-gcc', 'devtoolset-2-binutils', 'devtoolset-2-gcc' ]: 
+    require  => Yumrepo['devtools2']
+  }
+  
   package { $modules:
     ensure   => present,
     provider => 'npm',
