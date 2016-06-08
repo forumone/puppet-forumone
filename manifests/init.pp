032c68c6a86303abf4a700c2d44e72e37ae05509
William Hurley <whurley@forumone.com>
2016-06-08 15:37:22 -0400

    Making /tmp/vagrant-cache world writable

--- manifests/init.pp
+++ manifests/init.pp
@@ -14,6 +14,7 @@ class forumone ($ports = [80, 443, 8080, 8081, 18983, 8983, 3306, 13306, 1080, 4
   # Vagrant cachier directory
   file { "/tmp/vagrant-cache":
     ensure  => "directory",
+    mode    => "1777"
   }
 
   file { "/home/vagrant/.bashrc":
